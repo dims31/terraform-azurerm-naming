e2d50cf00d5577f35de83adee7558d7525f30441
Dimitri PUYET <dimitri.puyet@talan.com>
2024-09-30 15:03:26 +0200

    ok

--- variables.tf
+++ variables.tf
@@ -1,37 +1,54 @@
 variable "env" {
+  type = string
   validation {
     condition = contains(["dev","uat","bch","prd"], var.env)
     error_message = "Value must be dev/uat/bch/prd"
   }
+  description = "Value must be dev/uat/bch/prd"
 }
 
 variable "bu" {
+  type = string
   validation {
     condition = contains(["ide","shr","ntw","bu1","bu2"], var.bu)
     error_message = "Value must be ide/shr/ntw/bu1/bu2"
   }
+  description = "value must be ide/shr/ntw/bu1/bu2"
 }
 
 variable "project" {
+  type = string
   validation {
   condition = length(var.project) == 3
-  error_message = "Le code projet doit etre sur 3 lettres"
+  error_message = "Value must be 3 characters"
   }
+  description = "value must be 3 characters"
 }
 
 variable "free_name" {
+  type = string
   validation {
   condition = length(var.free_name) >= 3 && length(var.free_name) <= 5
-  error_message = "Le code projet doit etre sur 3 lettres"
+  error_message = "Free Name must be between 3 and 5 characters"
   }
   default = "001"
+  description = "Last 3 to 5 characters of the resource name"
 }
 
 variable "rsg_free_name" {
+  type = string
+  validation {
+    condition = length(var.rsg_free_name) >= 3 && length(var.rsg_free_name) <= 5
+    error_message = "Free Name must be between 3 and 5 characters"
+  }
   default = "app"
+  description = "Last 3 to 5 characters of the resource group name"
+
 }
 
 variable "legacy_name" {
+  type = string
   default = null
+  description = "If set, will override the naming convention"
 }
 
